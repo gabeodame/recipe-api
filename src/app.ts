345ff09231549493bb1d96a8dc42dc05420bd78a
Gabriel Odame <gabeodame@gmail.com>
2024-08-22 10:05:32 -0500

    install and configure cors

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,7 @@ import { json } from "body-parser";
 import { allRecipesRouter } from "./routes/allRecipes";
 import { recipeDetailsRouter } from "./routes/recipeDetails";
 import { newRecipesRouter } from "./routes/newRecipe";
-
-// import cors from "cors";
+import cors from "cors";
 
 const app = express();
 
@@ -14,6 +13,17 @@ app.set("trust proxy", true); //trust traffic coming from ingress-ngnix
 
 app.use(express.json()); // express.json() is a middleware that parses incoming requests with JSON payloads
 app.use(json());
+
+// Dynamic CORS setup
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      callback(null, origin || "*");
+    },
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 app.use(
   cookieSession({
